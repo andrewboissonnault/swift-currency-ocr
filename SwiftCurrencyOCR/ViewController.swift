4f76f06c93e5afd9605ceb8a0e863701676ff2f3
Andrew Boissonnault <aboissonnault@amstonstudio.com>
2016-03-17 17:26:50 +0100

    Initial Commit

---
+++ SwiftCurrencyOCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCurrencyOCR
+//
+//  Created by Andrew Boissonnault on 3/17/16.
+//  Copyright © 2016 Andrew Boissonnault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
